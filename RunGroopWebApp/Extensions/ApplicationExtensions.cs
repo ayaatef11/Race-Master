b131a51ff9ac2b785ee0f2e2bf28ccf87ecbb1c7
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-01 02:13:00 +0300

    register services

---
+++ RunGroopWebApp/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Repository;
+using RunGroopWebApp.Services;
+
+namespace RunGroopWebApp.Extensions
+{
+    public  static class ApplicationExtensions
+    {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection Services) { 
+
+    Services.AddScoped<IClubRepository, ClubRepository>();
+    Services.AddScoped<IRaceRepository, RaceRepository>();
+    Services.AddScoped<IDashboardRepository, DashboardRepository>();
+    Services.AddScoped<IUserRepository, UserRepository>();
+    Services.AddScoped<ILocationService, LocationService>();
+    Services.AddScoped<IPhotoService, PhotoService>();
+                return Services;
+    }
+}}
