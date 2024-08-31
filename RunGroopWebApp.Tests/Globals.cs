61f991fde2df7baa3af2b424fe70c5a6da057bad
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-01 02:12:39 +0300

    collect global namespaces

---
+++ RunGroopWebApp.Tests/Globals.cs
@@ -0,0 +1,9 @@
+﻿global using System.Threading.Tasks;
+global using Xunit;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using FakeItEasy;
+global using FluentAssertions;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
