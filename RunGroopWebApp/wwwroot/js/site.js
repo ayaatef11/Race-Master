9372babe6314e7ec529c6b3e0d090d8ae937f532
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-09 19:37:53 +0300

    add site localization

---
+++ RunGroopWebApp/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿function readNotification(id, target) {
+    $.ajax({
+        url: "/Notification/ReadNotification",
+        method: "GET",
+        data: { notificationId: id },
+        success: function (result) {
+            getNotification();
+
+            $(target).fade0ut('slow');
+
+            error: function(error) {
+                console.log(error);
+            }
+        })
+}
+                getNotification();
+
+                let connection = new signalR.HubConnection("/signalServer");
+
+                connection.on('displayNotification', () => {
+                    getNotification();
+
+                });
+
+                connection.start();
+            });
