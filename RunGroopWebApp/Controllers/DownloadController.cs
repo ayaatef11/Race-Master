b198e6bf495a6cfdb60118f9dd3a62663069600e
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-18 00:04:40 +0300

    upload file

---
+++ RunGroopWebApp/Controllers/DownloadController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace RunGroopWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class DownloadController : Controller
+    {
+        [HttpPost("FileUpload")]
+        public async Task< IActionResult> Get(List<IFormFile> files)
+        {
+            var size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+
+                if (formFile.Length > 0)
+                {
+
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), formFile.FileName);
+                    filePaths.Add(filePath);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+
+                        await formFile.CopyToAsync(stream);
+
+                }
+            }
+                return Ok(new { files.Count, size, filePaths });
+            
+        }
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+   
+}
